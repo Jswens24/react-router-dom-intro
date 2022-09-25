fd4c3fd7938bcb6000b7c0355025a079f17ae862
Jessica Swenson <jessicamswens@gmail.com>
2022-09-25 10:54:34 -0600

    practice with Route, Switch, Link, NavLink, Redirect

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,17 @@
+import { useParams } from 'react-router-dom';
+import React from 'react';
+
+const ProductDetail = () => {
+    const params = useParams();
+
+
+
+    return (
+        <section>
+            <h1>Product Detail</h1>
+            <p>{params.productId}</p>
+        </section>
+    )
+};
+
+export default ProductDetail;
